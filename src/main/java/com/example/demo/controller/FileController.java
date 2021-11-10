62bedb40da8da4b3ebc0a1624f69e8b03e041788
Ronny Peña <ra.pp.91.15@gmail.com>
2021-11-10 14:33:38 +0100

    Update FileController

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.model.File;
 import com.example.demo.repository.FileRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,9 +35,13 @@ public class FileController {
     }
 
     @GetMapping("/{id}")
-    public byte[] getFileId(@PathVariable UUID id) {
-        File file = fileRepository.getById(id);
-        return file.data;
+    public ResponseEntity<byte[]> getFile(@PathVariable UUID id) {
+        File file = fileRepository.findById(id).orElse(null);
+        if (file == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(file.contenttype))
+                .contentLength(file.data.length)
+                .body(file.data);
     }
 
 }
