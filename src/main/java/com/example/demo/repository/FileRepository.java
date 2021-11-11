43e7daf8e4c6f6959e84328ad2ef4e3ec17a9651
Ronny Peña <ra.pp.91.15@gmail.com>
2021-11-11 20:10:43 +0100

    Add pages to upload images

--- src/main/java/com/example/demo/repository/FileRepository.java
+++ src/main/java/com/example/demo/repository/FileRepository.java
@@ -1,10 +1,14 @@
+
 package com.example.demo.repository;
 
 import com.example.demo.domain.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FileRepository extends JpaRepository<File, UUID> {
-
+    @Query("select fileid from File")
+    List<String> getFileIds();
 }
