1351011ce83252affea9bddbca4e3f580d2a3d65
Ronny Peña <ra.pp.91.15@gmail.com>
2021-11-11 20:10:43 +0100

    Add pages to upload images

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,28 +1,27 @@
-package com.example.demo.controller;
 
+package com.example.demo.controller;
 
 import com.example.demo.domain.model.File;
 import com.example.demo.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
 
-    private final FileRepository fileRepository;
-    FileController(FileRepository fileRepository){
-        this.fileRepository = fileRepository;
-    }
+    @Autowired
+    private FileRepository fileRepository;
 
     @PostMapping
     public String upload(@RequestParam("file") MultipartFile uploadedFile) {
         try {
-            System.out.println(uploadedFile.getOriginalFilename() + ", " + uploadedFile.getContentType());
             File file = new File();
             file.contenttype = uploadedFile.getContentType();
             file.data = uploadedFile.getBytes();
@@ -34,14 +33,35 @@ public class FileController {
         }
     }
 
+
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> getFile(@PathVariable UUID id) {
         File file = fileRepository.findById(id).orElse(null);
+
         if (file == null) return ResponseEntity.notFound().build();
+
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(file.contenttype))
                 .contentLength(file.data.length)
                 .body(file.data);
     }
 
+
+    @GetMapping
+    public String hack() {
+        List<String> files = fileRepository.getFileIds();
+
+        String filesStr = "";
+        for (String file : files) {
+            filesStr += "<img src='/files/"+file+"' style='width:15em'>";
+        }
+
+        return "<form method=\"POST\" enctype=\"multipart/form-data\" style=\"display:flex;\">\n" +
+                "    <input id=\"file\" type=\"file\" name=\"file\" style=\"display:none\" onchange=\"document.getElementById('preview').src=window.URL.createObjectURL(event.target.files[0])\">\n" +
+                "    <label for=\"file\" style=\"border: 1px dashed #999\">\n" +
+                "        <img id=\"preview\" src=\"\" style=\"width:64px;\">\n" +
+                "    </label>\n" +
+                "    <input type=\"submit\" style=\"background:#0096f7;color: white;border: 0;border-radius: 3px;padding: 8px;\" value=\"Upload\">\n" +
+                "</form>\n <div style='display:flex;flex-wrap:wrap;gap:1em;'>" + filesStr + "</div>";
+    }
 }
--- src/main/resources/db/migration/V2__filetable.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE file (
-    fileid UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    contenttype TEXT,
-    data bytea);
